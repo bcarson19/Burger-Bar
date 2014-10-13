8fa5a7fee45af38a272a1a51824cac6e3c0112d6
Tom Kennedy <tkennedy@smu.edu>
2014-10-13 18:41:38 -0500

    wrapped fucntion in function

--- js/index.js
+++ js/index.js
@@ -2,18 +2,22 @@ console.log("in index javascript");
 var rootURL = "http://localhost:8888/Burger-Bar/index.php";
 
 /*Display either login form or past order*/
+changeLoginOrRecent();
 
+function changeLoginOrRecent(){
     if (localStorage.username) {
-    	console.log("username exists");
+    	console.log("username exists"); //display recent order
         document.getElementById("login").style.display = "none";
         document.getElementById("lastOrder").style.display = "block";
+
+
     }
     else {
-    	console.log("no username");
+    	console.log("no username"); //display Login screen
         document.getElementById("lastOrder").style.display = "none";
         document.getElementById("login").style.display = "block";
     }
-
+}
 
 
 $("#loginButton").click(function(){
